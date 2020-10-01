3de2986511cec87caa538a6b792c8dfe38a44738
kasungayan <kbandara149@gmail.com>
2020-10-01 20:10:16 +1000

    Code repo for MSOM project

---
+++ Classification/classification_analysis.py
@@ -0,0 +1,251 @@
+import pandas as pd
+import numpy as np
+import plotly
+import shap
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import mean_squared_error,roc_auc_score,precision_score
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans
+
+import pickle
+
+np.random.seed(123)
+
+shap_v1 = pd.read_feather('preprocessed_data/click_data_classification_correct2.ft')
+
+shap_v1_cluster = shap_v1[['skuID', 'skutable-attribute1', 'skutable-attribute2']]
+shap_v1_cluster_group_skuid = shap_v1_cluster_group['skuID']
+
+shap_v1_cluster_group_attributes = shap_v1_cluster_group[['skutable-attribute1', 'skutable-attribute2']]
+
+scaler = MinMaxScaler()
+
+shap_v1_cluster_group_attributes[['skutable-attribute1', 'skutable-attribute2']] = scaler.fit_transform(shap_v1_cluster_group_attributes[['skutable-attribute1', 'skutable-attribute2']])
+
+distortions = []
+K = range(1,10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k)
+    kmeanModel.fit(shap_v1_cluster_group_attributes)
+    distortions.append(kmeanModel.inertia_)
+    
+ plt.figure(figsize=(16,8))
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal k')
+plt.show()
+
+kmeanModel = KMeans(n_clusters=4)
+kmeanModel.fit(shap_v1_cluster_group_attributes)
+
+shap_v1_cluster_group_attributes['class']=kmeanModel.predict(shap_v1_cluster_group_attributes)
+y_kmeans = shap_v1_cluster_group_attributes['class']
+y_kmeans = np.array(y_kmeans)
+
+
+X = shap_v1_cluster_group_attributes
+
+#6 Visualising the clusters
+plt.scatter(X.loc[y_kmeans==0, 'skutable-attribute1'], X.loc[y_kmeans==0, 'skutable-attribute2'], s=100, c='red', label ='Cluster 1')
+plt.scatter(X.loc[y_kmeans==1, 'skutable-attribute1'], X.loc[y_kmeans==1, 'skutable-attribute2'], s=100, c='blue', label ='Cluster 2')
+plt.scatter(X.loc[y_kmeans==2, 'skutable-attribute1'], X.loc[y_kmeans==2, 'skutable-attribute2'], s=100, c='green', label ='Cluster 3')
+plt.scatter(X.loc[y_kmeans==3, 'skutable-attribute1'], X.loc[y_kmeans==3, 'skutable-attribute2'], s=100, c='cyan', label ='Cluster 4')
+#plt.scatter(X.loc[y_kmeans==4, 'attribute1'], X.loc[y_kmeans==4, 'attribute2'], s=100, c='cyan', label ='Cluster 5')
+#Plot the centroid. This time we're going to use the cluster centres  #attribute that returns here the coordinates of the centroid.
+plt.scatter(kmeanModel.cluster_centers_[:, 0], kmeanModel.cluster_centers_[:, 1], s=300, c='yellow', label = 'Centroids')
+plt.title('Clusters of Products')
+plt.xlabel('Atrribute 1')
+plt.ylabel('Atrribute 2')
+plt.show()
+
+final_df = pd.concat([shap_v1_cluster_group_skuid, shap_v1_cluster_group_attributes], axis = 1)
+
+shap_v1['skuID'] = shap_v1['skuID'].astype(str)
+final_df['skuID'] = final_df['skuID'].astype(str)
+
+df_final_joined = shap_v1.merge(final_df, on = "skuID", how = "right")
+
+shap_v1_filter = df_final_joined.copy()
+shap_v1_filter['class'] = shap_v1_filter['class'].astype('int8')
+
+shap_v1_filter = shap_v1_filter.drop(columns = ['skuID','skutable-attribute1', 'skutable-attribute2','quantity'])
+
+shap_v1 = shap_v1_filter.copy()
+
+age_categories = ['U','<=15', '16-25', '26-35', '36-45', '46-55', '>=56']
+#ordertablepromise_categories = ['nilorder','1', '2', '3', '4', '5', '6', '7', '8']
+#ordertable_gift_item_categories = ['nilorder','0', '1']
+#newtable_hasdiscount_categories = ['nilnewtable','yes', 'no']
+userlevel_categories = [-1, 0,1, 2, 3, 4, 10]
+education_categories = [-1, 1, 2, 3, 4]
+citylevel_categories = [-1, 1, 2, 3, 4, 5]
+purchastingpower_categories = [-1, 1, 2, 3, 4, 5]
+
+shap_v1["age"] = shap_v1["age"].astype("category", ordered=True,categories=age_categories).cat.codes
+#shap_v1["promise"] = shap_v1["promise"].astype("category", ordered=True,categories=ordertablepromise_categories).cat.codes
+#shap_v1["ordertable-gift_item"] = shap_v1["ordertable-gift_item"].astype("category", ordered=True,categories=ordertable_gift_item_categories).cat.codes
+#shap_v1["newtable-hasdiscount"] = shap_v1["newtable-hasdiscount"].astype("category", ordered=True, categories=newtable_hasdiscount_categories).cat.codes
+shap_v1["user_level"] = shap_v1["user_level"].astype("category", ordered=True,categories=userlevel_categories).cat.codes
+shap_v1["education"] = shap_v1["education"].astype("category", ordered=True,categories=education_categories).cat.codes
+shap_v1["city_level"] = shap_v1["city_level"].astype("category", ordered=True,categories=citylevel_categories).cat.codes
+shap_v1["purchase_power"] = shap_v1["purchase_power"].astype("category", ordered=True,categories=purchastingpower_categories).cat.codes
+
+geneder_categories = ['U', 'F', 'M']
+channel_categories = ['app', 'wechat', 'pc', 'mobile', 'others']
+marital_categories = ['U', 'M', 'S']
+
+shap_v1["gender"] = shap_v1["gender"].astype("category", ordered=True,categories=geneder_categories).cat.codes
+
+shap_v1["channel"] = shap_v1["channel"].astype("category", ordered=True,categories=channel_categories).cat.codes
+
+shap_v1["marital_status"] = shap_v1["marital_status"].astype("category", ordered=True,categories=marital_categories).cat.codes
+
+
+shap_v1['user_plus'] = shap_v1['user_plus'].astype('int8')
+shap_v1['day_of_week'] = shap_v1['day_of_week'].astype('int8')
+shap_v1['hour_of_day'] = shap_v1['hour_of_day'].astype('int8')
+shap_v1['product_type'] = shap_v1['product_type'].astype('int8')
+shap_v1['promise'] = pd.to_numeric(shap_v1['promise'], errors = 'coerce')
+shap_v1['number_of_gifts'] = shap_v1['number_of_gifts'].astype('float64')
+
+y = shap_v1['class']
+X = shap_v1.drop(columns=["class"])
+
+# specify your configurations as a dict
+params = {
+    'boosting_type': 'gbdt',
+    'objective': 'multiclass',
+    'num_class':4,
+    'metric': 'multi_logloss',
+    'num_leaves': 31,
+    'learning_rate': 0.04,
+   'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+    'verbose': 1,
+    'seed': 8
+}
+
+
+random_state = 7
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.1, random_state=random_state)
+d_train = lgb.Dataset(X_train, label=y_train)
+d_test = lgb.Dataset(X_test, label=y_test)
+
+model = lgb.train(params, d_train, valid_sets=[d_test], early_stopping_rounds=50)
+y_pred = model.predict(X_test)
+
+filename = 'finalized_correct_classification.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+# load the model from disk
+#loaded_model = pickle.load(open(filename, 'rb'))
+
+
+#argmax() method 
+y_pred_1 = [np.argmax(line) for line in y_pred]
+
+#using precision score for error metrics
+precision_score(y_pred_1,y_test,average=None).mean()
+
+gnb = GaussianNB()
+y_pred_naive = gnb.fit(X_train, y_train).predict(X_test)
+
+neigh = KNeighborsClassifier(n_neighbors=3)
+knn_fit = neigh.fit(X_train, y_train)
+y_pred_knn = knn_fit.predict(X_test)
+
+#using precision score for error metrics
+precision_score(y_pred_naive,y_test,average=None).mean()
+
+#using precision score for error metrics
+precision_score(y_pred_knn,y_test,average=None).mean()
+
+explainer = shap.TreeExplainer(model)
+
+shap_values = explainer.shap_values(X_test)
+
+shap.summary_plot(shap_values, X_test)
+
+shap.summary_plot(shap_values, features=X_test, class_inds=[0])
+shap.summary_plot(shap_values, features=X_test, class_inds=[1])
+shap.summary_plot(shap_values, features=X_test, class_inds=[2])
+shap.summary_plot(shap_values, features=X_test, class_inds=[3])
+
+shap.summary_plot(shap_values[0], features=X_test)
+shap.summary_plot(shap_values[1], features=X_test)
+shap.summary_plot(shap_values[2], features=X_test)
+shap.summary_plot(shap_values[3], features=X_test)
+
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[0], X_test, interaction_index=None)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[0], X_test)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[1], X_test, interaction_index=None)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[1], X_test)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[2], X_test, interaction_index=None)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[2], X_test)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[3], X_test, interaction_index=None)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values[3], X_test)
+
+X_test_disp = X_test.copy()
+
+X_test_disp['age'] = X_test_disp['age'].map(dict(enumerate(age_categories)))
+#X_test_disp['ordertable-gift_item'] = X_test_disp['ordertable-gift_item'].map(dict(enumerate(ordertable_gift_item_categories)))
+#X_test_disp['newtable-hasdiscount'] = X_test_disp['newtable-hasdiscount'].map(dict(enumerate(newtable_hasdiscount_categories)))
+X_test_disp['user_level'] = X_test_disp['user_level'].map(dict(enumerate(userlevel_categories)))
+X_test_disp['education'] = X_test_disp['education'].map(dict(enumerate(education_categories)))
+X_test_disp['city_level'] = X_test_disp['city_level'].map(dict(enumerate(citylevel_categories)))
+X_test_disp['purchase_power'] = X_test_disp['purchase_power'].map(dict(enumerate(purchastingpower_categories)))
+
+
+geneder_categories = ['U', 'F', 'M']
+channel_categories = ['app', 'wechat', 'pc', 'mobile', 'others']
+marital_categories = ['U', 'M', 'S']
+
+
+X_test_disp['gender'] = X_test_disp['gender'].map(dict(enumerate(geneder_categories)))
+X_test_disp['channel'] = X_test_disp['channel'].map(dict(enumerate(channel_categories)))
+X_test_disp['marital_status'] = X_test_disp['marital_status'].map(dict(enumerate(marital_categories)))
+
+# load JS visualization code to notebook
+shap.initjs() 
+
+plt.clf()
+shap.force_plot(explainer.expected_value[1], shap_values[1][16,:], X_test_disp.iloc[16,:], link="logit", matplotlib=True,show=False,  figsize=(20,4))
+plt.savefig("class2_classification_latest.png",bbox_inches='tight')
+
+shap.decision_plot(explainer.expected_value[1], shap_values[1][16,:], X_test_disp.iloc[16,:], link="logit")
+
+class0 = ['ordertable-original_unit_price', 'percentage_direct_discount_per_unit', 'product_type',
+         'percentage_quantity_discount_per_unit', 'promise', 'number_of_gifts', 'percentage_coupon_discount_per_unit']
+         
+class1 = ['ordertable-original_unit_price', 'percentage_direct_discount_per_unit', 'product_type',
+         'percentage_quantity_discount_per_unit', 'promise', 'channel', 'percentage_bundle_discount_per_unit']
+         
+class2 = ['ordertable-original_unit_price', 'percentage_direct_discount_per_unit', 'product_type',
+         'education', 'promise', 'percentage_coupon_discount_per_unit', 'percentage_quantity_discount_per_unit']
+         
+class3 = ['ordertable-original_unit_price', 'percentage_direct_discount_per_unit', 'product_type',
+         'percentage_bundle_discount_per_unit', 'promise', 'percentage_coupon_discount_per_unit', 'percentage_quantity_discount_per_unit']
+
+plt.clf()
+
+for i in class0: 
+    interaction_name = i
+    with_interaction = "class1_" + interaction_name + "_with_interaction" + ".png"
+    shap.dependence_plot(interaction_name, shap_values[1], X_test,show=False)
+    plt1.savefig(with_interaction,bbox_inches='tight') 
+
+for i in class0: 
+    interaction_name = i
+    with_interaction = "class1_" + interaction_name + "_without_interaction" + ".png"
+    shap.dependence_plot(interaction_name, shap_values[1], X_test,show=False, interaction_index=None)
+    plt1.savefig(with_interaction,bbox_inches='tight') 
+
---
+++ Classification/regression_task.py
@@ -0,0 +1,191 @@
+import pandas as pd
+import numpy as np
+import plotly
+import matplotlib.pyplot as plt
+import shap
+import lightgbm as lgb
+
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn import metrics
+
+from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
+
+np.random.seed(123)
+
+shap_v1 = pd.read_feather('preprocessed_data/click_data_shap_final_version.ft')
+
+shap_v1['percentage_direct_discount_per_unit'] = shap_v1['ordertable-direct_discount_per_unit']/shap_v1['ordertable-original_unit_price']
+shap_v1['percentage_quantity_discount_per_unit'] = shap_v1['ordertable-quantity_discount_per_unit']/shap_v1['ordertable-original_unit_price']
+shap_v1['percentage_bundle_discount_per_unit'] = shap_v1['ordertable-bundle_discount_per_unit']/shap_v1['ordertable-original_unit_price']
+shap_v1['percentage_coupon_discount_per_unit'] = shap_v1['ordertable-coupon_discount_per_unit']/shap_v1['ordertable-original_unit_price']
+
+shap_v1 = shap_v1.drop(columns = ['ordertable-direct_discount_per_unit', 'ordertable-quantity_discount_per_unit', 'ordertable-bundle_discount_per_unit','ordertable-coupon_discount_per_unit'])
+
+shap_v1.rename(columns = {'usertable-age':'age', 
+                         'ordertable-promise':'promise',
+                          'usertable-user_level':'user_level',
+                          'usertable-education':'education',
+                          'usertable-city_level':'city_level',
+                          'usertable-purchase_power':'purchase_power',
+                          'usertable-gender':'gender',
+                          'clicktable-channel':'channel',
+                          'usertable-marital_status':'marital_status'
+                         }, inplace = True)
+                         
+                         
+shap_v1.rename(columns = {'skutable-type':'product_type', 
+                         'skutable-attribute1':'product_attribute1',
+                          'skutable-attribute2':'product_attribute2',
+                          'usertable-plus':'user_plus',
+                          'ordertable-number_of_gifts':'number_of_gifts'
+                         }, inplace = True)
+                         
+shap_v1['promise'] =shap_v1['promise'].replace({'nilorder': np.nan})
+
+age_categories = ['U','<=15', '16-25', '26-35', '36-45', '46-55', '>=56']
+#ordertablepromise_categories = ['nilorder','1', '2', '3', '4', '5', '6', '7', '8']
+#ordertable_gift_item_categories = ['nilorder','0', '1']
+#newtable_hasdiscount_categories = ['nilnewtable','yes', 'no']
+userlevel_categories = [-1, 0,1, 2, 3, 4, 10]
+education_categories = [-1, 1, 2, 3, 4]
+citylevel_categories = [-1, 1, 2, 3, 4, 5]
+purchastingpower_categories = [-1, 1, 2, 3, 4, 5]
+
+shap_v1["age"] = shap_v1["age"].astype("category", ordered=True,categories=age_categories).cat.codes
+#shap_v1["promise"] = shap_v1["promise"].astype("category", ordered=True,categories=ordertablepromise_categories).cat.codes
+#shap_v1["ordertable-gift_item"] = shap_v1["ordertable-gift_item"].astype("category", ordered=True,categories=ordertable_gift_item_categories).cat.codes
+#shap_v1["newtable-hasdiscount"] = shap_v1["newtable-hasdiscount"].astype("category", ordered=True, categories=newtable_hasdiscount_categories).cat.codes
+shap_v1["user_level"] = shap_v1["user_level"].astype("category", ordered=True,categories=userlevel_categories).cat.codes
+shap_v1["education"] = shap_v1["education"].astype("category", ordered=True,categories=education_categories).cat.codes
+shap_v1["city_level"] = shap_v1["city_level"].astype("category", ordered=True,categories=citylevel_categories).cat.codes
+shap_v1["purchase_power"] = shap_v1["purchase_power"].astype("category", ordered=True,categories=purchastingpower_categories).cat.codes
+
+
+geneder_categories = ['U', 'F', 'M']
+channel_categories = ['app', 'wechat', 'pc', 'mobile', 'others']
+marital_categories = ['U', 'M', 'S']
+
+shap_v1["gender"] = shap_v1["gender"].astype("category", ordered=True,categories=geneder_categories).cat.codes
+
+shap_v1["channel"] = shap_v1["channel"].astype("category", ordered=True,categories=channel_categories).cat.codes
+
+shap_v1["marital_status"] = shap_v1["marital_status"].astype("category", ordered=True,categories=marital_categories).cat.codes
+
+
+shap_v1['user_plus'] = shap_v1['user_plus'].astype('int8')
+shap_v1['day_of_week'] = shap_v1['day_of_week'].astype('int8')
+shap_v1['hour_of_day'] = shap_v1['hour_of_day'].astype('int8')
+shap_v1['product_type'] = shap_v1['product_type'].astype('int8')
+shap_v1['product_type'] = shap_v1['product_type'].astype('int8')
+shap_v1['promise'] = pd.to_numeric(shap_v1['promise'], errors = 'coerce')
+
+y = shap_v1['quantity']
+X = shap_v1.drop(columns=["quantity"])
+
+random_state = 7
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=random_state)
+d_train = lgb.Dataset(X_train, label=y_train)
+d_test = lgb.Dataset(X_test, label=y_test)
+
+
+#build the lightgbm model
+params = {
+          "objective" : "regression",
+          "metric" :"rmse",
+          "force_row_wise" : True,
+          "learning_rate" : 0.075,
+          "sub_row" : 0.75,
+          "bagging_freq" : 1,
+          "lambda_l2" : 0.1,
+          "metric": ["rmse"],
+          'verbosity': 1,
+          'num_iterations' : 1200,
+          'num_leaves': 128,
+          "min_data_in_leaf": 100,
+         }
+         
+         
+model = lgb.train(params, d_train, valid_sets=[d_test], early_stopping_rounds=50)
+
+explainer = shap.TreeExplainer(model)
+X_test_disp = X_test.copy()
+
+X_test_disp['age'] = X_test_disp['age'].map(dict(enumerate(age_categories)))
+#X_test_disp['ordertable-gift_item'] = X_test_disp['ordertable-gift_item'].map(dict(enumerate(ordertable_gift_item_categories)))
+#X_test_disp['newtable-hasdiscount'] = X_test_disp['newtable-hasdiscount'].map(dict(enumerate(newtable_hasdiscount_categories)))
+X_test_disp['user_level'] = X_test_disp['user_level'].map(dict(enumerate(userlevel_categories)))
+X_test_disp['education'] = X_test_disp['education'].map(dict(enumerate(education_categories)))
+X_test_disp['city_level'] = X_test_disp['city_level'].map(dict(enumerate(citylevel_categories)))
+X_test_disp['purchase_power'] = X_test_disp['purchase_power'].map(dict(enumerate(purchastingpower_categories)))
+
+eneder_categories = ['U', 'F', 'M']
+channel_categories = ['app', 'wechat', 'pc', 'mobile', 'others']
+marital_categories = ['U', 'M', 'S']
+
+
+X_test_disp['gender'] = X_test_disp['gender'].map(dict(enumerate(geneder_categories)))
+X_test_disp['channel'] = X_test_disp['channel'].map(dict(enumerate(channel_categories)))
+X_test_disp['marital_status'] = X_test_disp['marital_status'].map(dict(enumerate(marital_categories)))
+
+shap_values = explainer.shap_values(X_test)
+
+shap.summary_plot(shap_values, X_test, plot_type="bar", max_display = 21)
+
+shap.summary_plot(shap_values,X_test,  max_display = 21)
+
+predictions = model.predict(X_test)
+predictions[predictions < 0] = 0
+
+actual = y_test
+
+print("RMSE Error LightGBM:{}".format(np.sqrt(metrics.mean_squared_error(actual,predictions))))
+
+
+X_test.rename(columns = {'ordertable-original_unit_price':'original_unit_price'},inplace = True)
+
+shap.dependence_plot("promise", shap_values, X_test, interaction_index=None)
+shap.dependence_plot("promise", shap_values, X_test)
+
+shap.dependence_plot("percentage_direct_discount_per_unit", shap_values, X_test)
+shap.dependence_plot("percentage_direct_discount_per_unit", shap_values, X_test, interaction_index=None)
+
+
+shap.dependence_plot("original_unit_price", shap_values, X_test)
+shap.dependence_plot("original_unit_price", shap_values, X_test, interaction_index=None)
+
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values, X_test)
+shap.dependence_plot("percentage_quantity_discount_per_unit", shap_values, X_test, interaction_index=None)
+
+shap.dependence_plot("percentage_bundle_discount_per_unit", shap_values, X_test)
+shap.dependence_plot("percentage_bundle_discount_per_unit", shap_values, X_test, interaction_index=None)
+
+shap.dependence_plot("percentage_coupon_discount_per_unit", shap_values, X_test)
+shap.dependence_plot("percentage_coupon_discount_per_unit", shap_values, X_test, interaction_index=None)
+
+shap.dependence_plot("user_level", shap_values, X_test)
+shap.dependence_plot("user_level", shap_values, X_test,interaction_index=None)
+
+shap.dependence_plot("education", shap_values, X_test)
+shap.dependence_plot("education", shap_values, X_test, interaction_index=None)
+
+# load JS visualization code to notebook
+shap.initjs()
+
+plt.clf()
+shap.force_plot(explainer.expected_value, shap_values[1594,:], X_test_disp.iloc[1594,:], matplotlib=True, show=False, figsize=(20,4))
+plt.savefig("prediction1.png",bbox_inches='tight')
+
+plt.clf()
+shap.force_plot(explainer.expected_value, shap_values[1594,:], X_test_disp.iloc[1594,:], matplotlib=True, show=False, figsize=(20,4))
+plt.savefig("prediction1.eps",bbox_inches='tight', format='eps')
+
+shap.decision_plot(explainer.expected_value, shap_values[1594,:], X_test_disp.iloc[1594,:])
+
+
+
+
+
+
+
